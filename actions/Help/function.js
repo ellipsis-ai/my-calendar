2f45c8a2015e8ca4db2c54f8e94fe4c4549eed07
Andrew Catton <>
2019-06-26 16:48:34 +0000

    Added action buttons to help

--- actions/Help/function.js
+++ actions/Help/function.js
@@ -1,3 +1,30 @@
 function(ellipsis) {
-  
+  ellipsis.success("", {
+  choices: [
+    {
+      actionName: 'Agenda',
+      label: 'Today',
+      allowMultipleSelections: true,
+      allowOthers: true
+    },
+    {
+      actionName: 'Reminders',
+      label: 'Now',
+      allowMultipleSelections: true,
+      allowOthers: true
+    },
+    {
+      actionName: 'Setup',
+      label: 'Setup',
+      allowMultipleSelections: true,
+      allowOthers: true
+    },
+    {
+      actionName: 'Deactivate',
+      label: 'Stop calendar',
+      allowMultipleSelections: true,
+      allowOthers: true
+    },
+  ]
+})
 }
